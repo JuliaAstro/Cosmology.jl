a917cbffa67f045b13f5c5c98f23babbac0abf58
Abhro R <5664668+abhro@users.noreply.github.com>
2024-07-15 12:18:29 -0400

    Add conditions for doctests in test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,14 @@ using Documenter
 
 @testset "Cosmology" begin
 
-    DocMeta.setdocmeta!(Cosmology, :DocTestSetup, :(using Cosmology); recursive = true)
-    doctest(Cosmology)
+    # Doctest is good for CI, but has variation in system to system. For now,
+    # only run on supported systems as a measure that the docs are reasonably
+    # accurate.
+    doctest_conds = Bool[Sys.islinux(), Sys.ARCH == :x86_64]
+    if all(doctest_conds)
+        DocMeta.setdocmeta!(Cosmology, :DocTestSetup, :(using Cosmology); recursive = true)
+        doctest(Cosmology)
+    end
 
     # values from http://icosmos.co.uk/
 
