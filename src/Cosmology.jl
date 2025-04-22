130b266d66dbdd12260c2c1ee4ab70d3bb140e08
abhro <5664668+abhro@users.noreply.github.com>
2025-04-22 13:18:27 -0400

    Wrap line

--- src/Cosmology.jl
+++ src/Cosmology.jl
@@ -191,8 +191,8 @@ end
     scale_factor(z)
 
 Return the scale factor ``a(t)`` for a given redshift ``z(t)``. According to
-the [Friedmann–Lemaître–Robertson–Walker metric](https://en.wikipedia.org/wiki/Friedmann–Lemaître–Robertson–Walker_metric) it's given as ``a = 1/(1 + z)``
-([Schneider 2015, p. 186](@cite schneider2015)).
+the [Friedmann–Lemaître–Robertson–Walker metric](https://en.wikipedia.org/wiki/Friedmann–Lemaître–Robertson–Walker_metric)
+it's given as ``a = 1/(1 + z)`` ([Schneider 2015, p. 186](@cite schneider2015)).
 
 A scale factor of 1, i.e., a redshift of 0, refers to the present epoch.
 """
