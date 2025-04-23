6bc463e58f2fa207159d1bf242ca91e6b05a459c
abhro <5664668+abhro@users.noreply.github.com>
2025-04-23 09:27:40 -0400

    Add math details about comoving_radial_dist

--- src/Cosmology.jl
+++ src/Cosmology.jl
@@ -311,12 +311,15 @@ Z
 
 comoving_radial_dist(c::AbstractCosmology, z₁, z₂ = nothing; kws...) = hubble_dist0(c) * Z(c, z₁, z₂; kws...)
 
-"""
+@doc raw"""
     comoving_radial_dist([u::Unitlike,] c::AbstractCosmology, [z₁,] z₂)
 
-Comoving radial distance in Mpc at redshift `z₂` as seen by an observer at `z₁`.
+Comoving radial distance (``D_C``) in Mpc at redshift `z₂` as seen by an observer at `z₁`.
 Redshift `z₁` defaults to 0 if omitted.  Will convert to compatible unit `u` if
 provided.
+
+It's calculated as ``D_C = D_{H0} Z``, where ``D_{H0}`` is the Hubble distance at
+the present epoch and, ``Z = \int_{z_1}^{z_2} \frac{dz}{E(z)}``.
 """
 comoving_radial_dist
 
